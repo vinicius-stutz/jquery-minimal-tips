c83df69067fe46f4039aac7298b1f56771c82cb4
Vinícius Stutz <vinicius-stutz@users.noreply.github.com>
2013-11-22 17:09:50 -0200

    Update jquery.minimalTips.js
    Code optimization

--- jquery.minimalTips.js
+++ jquery.minimalTips.js
@@ -5,7 +5,7 @@
 /**
 *
 * http://www.vinicius-stutz.com/
-* Version: 0.1 of 19 Nov 2013
+* Version: 0.2 of 22 Nov 2013
 * Copyright 2013 - Vinícius Stutz
 * Licensed under the MIT license
 *
@@ -20,7 +20,7 @@
 *   - Put CSS file "jquery.minimalTips.css" in your code;
 *   - Enjoy!
 *
-* Very easy to use! Too light to load (only 5 KB, this plugin and your style together)!
+* Very easy to use! Too light to load (only 6 KB, this plugin and your style together)!
 *
 **/
 
@@ -35,7 +35,31 @@ $.minimalTips = function(){
 
   var largura;
   var altura;
-  
+
+  // Cuida do posicionamento
+  function setPos(e){
+    largura = $("#MinimalTip").width();
+    altura = $("#MinimalTip").height();
+    
+    posX = ((e.pageX + largura + (yOffset*2)) > $(window).width()) ? e.pageX - (largura + 50) + yOffset : e.pageX + yOffset;
+
+    if((e.pageY + altura + (xOffset*2)) > $(window).height()){
+      posY = e.pageY - (altura + 40) + xOffset;
+      $("#MinimalTip").removeClass('arrow-up');
+      $("#MinimalTip").addClass('arrow-down');
+    } else {
+      posY = e.pageY + xOffset;
+      $("#MinimalTip").removeClass('arrow-down');
+      $("#MinimalTip").addClass('arrow-up');
+    }
+
+    $("#MinimalTip")
+      .css("top", posY + "px")
+      .css("left", posX + "px")
+      .fadeIn("fast"); // Você também pode mudar a velocidade da transição
+  }
+
+  // Quando o cursor estiver sobre o elemento...
   $("a, .mintip").hover(function(e){
   
     if (this.title != "") { this.t = this.title; }
@@ -44,57 +68,17 @@ $.minimalTips = function(){
     this.title = "";
     
     $("body").append("<div id='MinimalTip'>"+ this.t +"</div>");
-    
-    largura = $("#MinimalTip").width();
-    altura = $("#MinimalTip").height();
-    
-    posX = ((e.pageX + largura + (yOffset*2)) > $(window).width()) ? e.pageX - (largura + 50) + yOffset : e.pageX + yOffset;
-
-    if((e.pageY + altura + (xOffset*2)) > $(window).height()){
-      posY = e.pageY - (altura + 40) + xOffset;
-      $("#MinimalTip").removeClass('arrow-up');
-      $("#MinimalTip").addClass('arrow-down');
-    } else {
-      posY = e.pageY + xOffset;
-      $("#MinimalTip").removeClass('arrow-down');
-      $("#MinimalTip").addClass('arrow-up');
-    }
-    
-    $("#MinimalTip")
-      .css("top", posY + "px")
-      .css("left", posX + "px")
-      .fadeIn("fast"); // Você também pode mudar a velocidade da transição
   
+    setPos(e);
+
     },
 
     function(){
       this.title = this.t;
       $("#MinimalTip").remove();
-    }
-
-  );
-
-  $("a, .mintip").mousemove(function(e){
-    
-    largura = $("#MinimalTip").width();
-    altura = $("#MinimalTip").height();
-    
-    posX = ((e.pageX + largura + (yOffset*2)) > $(window).width()) ? e.pageX - (largura + 50) + yOffset : e.pageX + yOffset;
-
-    if((e.pageY + altura + (xOffset*2)) > $(window).height()){
-      posY = e.pageY - (altura + 40) + xOffset;
-      $("#MinimalTip").removeClass('arrow-up');
-      $("#MinimalTip").addClass('arrow-down');
-    } else {
-      posY = e.pageY + xOffset;
-      $("#MinimalTip").removeClass('arrow-down');
-      $("#MinimalTip").addClass('arrow-up');
-    }
-
-    $("#MinimalTip")
-      .css("top", posY + "px")
-      .css("left", posX + "px");
-
   });
+
+  // Ao mover o cursor...
+  $("a, .mintip").mousemove(function(e){ setPos(e); });
 
 };
